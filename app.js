3756958829da9cf3940ac2344d5e834a2bfdd1de
Rkomi98 <mirko-98@libero.it>
2025-07-30 10:52:32 +0200

    Non ancora sicuro

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 // app.js
 // Core logic for the hackathon voting game.
 
-// -- Firebase configuration is now in firebase-config.js --
+// Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBrqgVL8EIbhZaNe4d4SAfVYhh9BttglIU",
+    authDomain: "gamificationapp-fd026.firebaseapp.com",
+    databaseURL: "https://gamificationapp-fd026-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "gamificationapp-fd026",
+    storageBucket: "gamificationapp-fd026.firebasestorage.app",
+    messagingSenderId: "872395447892",
+    appId: "1:872395447892:web:f507c72c8aa99b354557c1",
+    measurementId: "G-F4LTRMNK1Y"
+};
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
