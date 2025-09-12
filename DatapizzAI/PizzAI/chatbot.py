1a9169db8e47adce18a9607f3996676af8b59b93
Rkomi98 <mirko-98@libero.it>
2025-09-12 10:28:00 +0200

    Create chatbot.py

---
+++ DatapizzAI/PizzAI/chatbot.py
@@ -0,0 +1,26 @@
+from datapizzai.clients import ClientFactory
+from datapizzai.memory import Memory
+from datapizzai.type import TextBlock, ROLE
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+class SimpleChatbot:
+    def __init__(self):
+        self.client = ClientFactory.create("google", os.getenv("GOOGLE_API_KEY"), "gemini-2.5-flash")
+        self.memory = Memory()
+    
+    def send_message(self, user_input: str) -> str:
+        self.memory.add_turn([TextBlock(content=user_input)], ROLE.USER)
+        response = self.client.invoke("", memory=self.memory)
+        self.memory.add_turn([TextBlock(content=response.text)], ROLE.ASSISTANT)
+        return response.text
+
+# Utilizzo
+bot = SimpleChatbot()
+
+while True:
+    user_input = input("Tu: ").strip()
+    if user_input.lower() in ["esci", "quit"]:
+        break
+    print("Bot:", bot.send_message(user_input))
