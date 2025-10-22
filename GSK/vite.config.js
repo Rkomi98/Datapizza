abaf33ca1780bbbc7b6bbbad17c9449daf8a77e3
Rkomi98 <mirko-98@libero.it>
2025-10-22 11:51:31 +0200

    Update vite.config.js

--- GSK/vite.config.js
+++ GSK/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const repoBase = '/GSK/'
+const repoBase = '/Datapizza/GSK/'
 
 export default defineConfig(({ command }) => ({
   base: command === 'serve' ? '/' : repoBase,
