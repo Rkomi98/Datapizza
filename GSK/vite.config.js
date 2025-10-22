772e6ae20800b5790f40132c4bb0001176e2676b
Rkomi98 <mirko-98@libero.it>
2025-10-22 11:25:31 +0200

    Proviamo

--- GSK/vite.config.js
+++ GSK/vite.config.js
@@ -1,13 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const repoBase = '/Datapizza/GSK/'
+const repoBase = '/GSK/'
 
 export default defineConfig(({ command }) => ({
   base: command === 'serve' ? '/' : repoBase,
   plugins: [react()],
   build: {
-    outDir: 'dist/GSK',
+    outDir: 'dist',
     emptyOutDir: true,
   },
 }))
